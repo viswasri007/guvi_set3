b37428602a9c950804215c1f88422ee24ede329f
viswasri007 <41729456+viswasri007@users.noreply.github.com>
2018-08-02 14:59:40 +0530

    Create numeric.java

---
+++ numeric.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		String a=s.next();
+		try
+		{
+			Integer.parseInt(a);
+			System.out.println("yes");
+		}
+		catch(NumberFormatException e)
+		{
+			System.out.println("No");
+		}
+	}
+}
