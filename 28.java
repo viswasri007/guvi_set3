d6798a89e50c1021c5f11621488c924a9e2cb06a
viswasri007 <41729456+viswasri007@users.noreply.github.com>
2018-08-03 14:17:43 +0530

    Create 28.java

---
+++ 28.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		a[i]=s.nextInt();
+		for(int i=0;i<n;i++)
+		System.out.println(a[i]+" "+i);
+	}
+}
